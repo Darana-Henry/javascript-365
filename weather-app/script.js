6c591df9cdfbc463b9450a3867f86bbbcc35f0f1
Henry Darana <h.darana@gmail.com>
2024-04-24 21:27:07 +0530

    Weather App Phase 3

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,10 +1,33 @@
-const apiKey = "";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=zurich&units=metric";
+const apiKey = "91c38a9f8d12ef9f764d989f74bf8707";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
-async function checkWeather() {
-    const response = await fetch(apiURL + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiURL + `&q=${city}` + `&appid=${apiKey}`);
     var data = await response.json();
     console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+    }
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
