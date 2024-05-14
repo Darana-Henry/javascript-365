72f9ffcc5b73bf96807b7f6eeacfb194ae2cc60b
Henry Darana <h.darana@gmail.com>
2024-05-14 21:30:26 +0530

    Guess My Number | Session 2

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const secret = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number !';
+  } else if (guess === secret) {
+    document.querySelector('.message').textContent = '🥳 Correct Number !';
+  } else if (score > 1) {
+    if (guess > secret) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else if (guess < secret) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent = '💥 You lost the game!';
+    document.querySelector('.score').textContent = 0;
+  }
+});
